bce96063e5db5ea080f6482783ca0f156dea3f6d
david95j2 <glory20220j@gmail.com>
2021-12-23 23:00:31 +0900

    global variation anther version

--- python_basic/main.py
+++ python_basic/main.py
@@ -1,10 +1,25 @@
 gun = 10
 
+# def checkpoint(soldiers):
+#   global gun
+#   gun = gun - soldiers
+#   print("[함수 내] 남은 총 : {}".format(gun))
+
+# print("전체 총 : {}".format(gun))
+# checkpoint(2)
+# print("남은 총 : {}".format(gun))
+
 def checkpoint(soldiers):
   global gun
   gun = gun - soldiers
   print("[함수 내] 남은 총 : {}".format(gun))
 
+def checkpoint_ret(gun, soldiers):
+  gun = gun - soldiers
+  print("[함수 내] 남은 총 : {}".format(gun))
+  return gun
+
 print("전체 총 : {}".format(gun))
-checkpoint(2)
+
+gun = checkpoint_ret(gun, 2)
 print("남은 총 : {}".format(gun))
